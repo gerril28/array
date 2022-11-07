34de653b764cd08cc6977e733fbfc59927f28172
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-07 09:32:53 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 4; index++) {
+        list.push(index)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let list: number[] = []
+list = []
+basic.forever(function () {
+	
+})
