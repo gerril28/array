a7c9646502c5dfad7ba5ab829a42dcee9b49258c
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-08 09:06:56 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 4; index++) {
-        list.push(index)
+    basic.showString("" + (list[0]))
+})
+input.onButtonPressed(Button.AB, function () {
+    p1 = list[0]
+    p2 = BadMen[1]
+    if (p1 == p1) {
+        basic.showString("" + (p1))
+        basic.showString("Win")
+    } else {
+        basic.showString("" + (p2))
+        basic.showString("Lose")
     }
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showString("" + (BadMen[1]))
 })
-let list: number[] = []
-list = []
-basic.forever(function () {
-	
-})
+let p2 = ""
+let p1 = ""
+let BadMen: string[] = []
+let list: string[] = []
+list = ["Spiderman", "Batman", "Lebron James"]
+BadMen = ["Thanos", "Joker", "Dr Doom"]
